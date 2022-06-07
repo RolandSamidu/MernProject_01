faa014499ca7588f7cecf8c2e9f74f318c88fec3
RolandSamidu <samidu.appuhamy@gmail.com>
2022-06-08 00:50:00 +0530

    Updated

--- mernweb/src/App.js
+++ mernweb/src/App.js
@@ -1,12 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/Navbar';
+import Home from './components/Home';
 
 function App() {
     return ( 
-        <div>
+        <>
            <Navbar/> 
-        </div>
+           <Home/>
+        </>
     );
 }
 
--- mernweb/src/components/Navbar.jsx
+++ mernweb/src/components/Navbar.jsx
@@ -5,7 +5,7 @@ const Navbar = () => {
         <div>
            <nav class="navbar navbar-expand-lg bg-light shadow">
   <div class="container">
-    <a class="navbar-brand" href="#">Navbar</a>
+    
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
@@ -15,27 +15,20 @@ const Navbar = () => {
           <a class="nav-link active" aria-current="page" href="#">Home</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#">Link</a>
-        </li>
-        <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Dropdown
-          </a>
-          <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-            <li><a class="dropdown-item" href="#">Action</a></li>
-            <li><a class="dropdown-item" href="#">Another action</a></li>
-            <li><hr class="dropdown-divider"/></li>
-            <li><a class="dropdown-item" href="#">Something else here</a></li>
-          </ul>
+          <a class="nav-link" href="#">About</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link disabled">Disabled</a>
+          <a class="nav-link" href="#">Services</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Contact</a>
         </li>
       </ul>
-      <form class="d-flex" role="search">
-        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
-        <button class="btn btn-outline-success" type="submit">Search</button>
-      </form>
+      <a class="navbar-brand fw-bolder fs-4 mx-auto text-black" href="#">Navbar</a>
+      <button className="btn btn-outline-primary ms-auto px-4 rounded-pill">
+        <i className='fa fa-sign-in me-2'></i> Login</button>
+      <button className="btn btn-outline-primary ms-2 px-4 rounded-pill">
+        <i className="fa fa-user-plus me-2"></i> Register</button>
     </div>
   </div>
 </nav>
