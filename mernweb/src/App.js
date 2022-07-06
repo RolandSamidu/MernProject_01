61cea79323b94cd25f22bb76c88d538c6cd49bdd
RolandSamidu <samidu.appuhamy@gmail.com>
2022-07-06 23:29:49 +0530

    Updated

--- mernweb/src/App.js
+++ mernweb/src/App.js
@@ -1,8 +1,9 @@
-import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/Navbar';
 import Home from './components/Home';
 import About from './components/About';
+import Services from './components/Services';
+import Contact from './components/Contact';
 
 function App() {
     return ( 
@@ -10,6 +11,8 @@ function App() {
            <Navbar/> 
            <Home/>
            <About/>
+           <Services/>
+           <Contact/>
         </>
     );
 }
