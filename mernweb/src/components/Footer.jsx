307166558c799c8df5eb51a764a37a3d1afb4af4
RolandSamidu <samidu.appuhamy@gmail.com>
2022-07-06 23:27:29 +0530

    Updated

---
+++ mernweb/src/components/Footer.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Footer = () => {
+    return(
+        <div>
+            <footer className='footer text-white'>
+                <div className='container'>
+                    <footer></footer>
+                </div>
+            </footer>
+        </div>
+    );
+}
+
+export default Footer;
--- mernweb/src/components/Navbar.jsx
+++ mernweb/src/components/Navbar.jsx
@@ -1,39 +1,39 @@
 import React from 'react';
 
 const Navbar = () => {
-    return(
-        <div>
-           <nav class="navbar navbar-expand-lg bg-light shadow">
-  <div class="container">
-    
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-        <li class="nav-item">
-          <a class="nav-link active text-black" aria-current="page" href="#">Home</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link text-black" href="#">About</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link text-black" href="#">Services</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link text-black" href="#">Contact</a>
-        </li>
-      </ul>
-      <a class="navbar-brand fw-bolder fs-4 mx-auto text-black" href="#">FINE ART</a>
-      <button className="btn btn-outline-primary ms-auto px-4 rounded-pill">
-        <i className='fa fa-sign-in me-2'></i> Login</button>
-      <button className="btn btn-outline-primary ms-2 px-4 rounded-pill">
-        <i className="fa fa-user-plus me-2"></i> Register</button>
-    </div>
-  </div>
-</nav>
+  return (
+    <div>
+      <nav class="navbar navbar-expand-lg bg-light shadow">
+        <div class="container">
+
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+              <li class="nav-item">
+                <a class="nav-link active text-black" aria-current="page" href="#">Home</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link text-black" href="#">About</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link text-black" href="#">Services</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link text-black" href="#">Contact</a>
+              </li>
+            </ul>
+            <a class="navbar-brand fw-bolder fs-4 mx-auto text-black" href="#">FINE ART</a>
+            <button className="btn btn-outline-primary ms-auto px-4 rounded-pill">
+              <i className='fa fa-sign-in me-2'></i> Login</button>
+            <button className="btn btn-outline-primary ms-2 px-4 rounded-pill">
+              <i className="fa fa-user-plus me-2"></i> Register</button>
+          </div>
         </div>
-    );
+      </nav>
+    </div>
+  );
 }
 
 export default Navbar;
